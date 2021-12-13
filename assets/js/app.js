b0793fd81908d1f722ed9fbd2427b2a15712b1f5
Lnastasiia <aluzyakina@gmail.com>
2021-12-13 19:52:11 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+
+let number = Math.floor(Math.random()*100);
+console.log (number);
+let count = 10;
+function guessNumber (){
+    if (count > 0) {
+
+        let userNum = document.getElementById('myNumber').value;
+        userNum = parseInt(userNum);
+        let decryptionPlace = document.getElementById('decryptionPlace');
+
+        if (userNum == number) {
+            decryptionPlace.innerHTML = 'Поздравляю!!! Вы угадали!!!';
+        }
+        else if (userNum > number) {
+            decryptionPlace.innerHTML = 'Ваше число больше моего!';
+        }
+        else if (userNum < number) {
+            decryptionPlace.innerHTML = 'Ваше число меньше моего!';
+        }
+        count = count - 1;
+        document.getElementById('count').innerHTML = count;
+    }
+    else {
+        alert ('Попытки закончились. Вы проиграли!');
+        location.reload ();
+    }
+   
+}
+
+
